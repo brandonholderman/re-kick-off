357703109c635e6a5b38d764ec18df5271e0ee60
fifthflow <brandonholderman@outlook.com>
2026-09-25 15:29:20 -0700

    fixed modal data order bug. was caused by an out of place div. verifying styles look correct for all screen sizes then moving to fix light mode styles

--- Kick-Off/src/components/RecipeDetails.jsx
+++ Kick-Off/src/components/RecipeDetails.jsx
@@ -16,22 +16,25 @@ const CloseIcon = () => (
 
 const RecipeDetails = (props) => {
     if (!props.show) return null;
-    const healthLabels = filterHealthLabels(props.data.recipe.healthLabels);
-    const nutritionInfo = props.data.recipe.digest;
+
+    const { recipe } = props.data;
+    const healthLabels = filterHealthLabels(recipe.healthLabels);
+    const nutritionInfo = recipe.digest;
+    const ingredientInfo = recipe.ingredientLines;
 
     return (
         <div className="modal" onClick={props.onClose}>
             <div className="modal-content" onClick={e => e.stopPropagation()}>
                 <div className="modal-header">
-                    <h4 className="modal-title">{props.data.recipe.label}</h4>
+                    <h4 className="modal-title">{recipe.label}</h4>
                     {/* <button onClick={props.onClose} className="modal-full-recipe"> */}
                         {/* <MdClose style={{ color: "#7b7b7b", width: "40px", height: "40px" }} /> */}
                         {/* <CloseIcon />
                     </button> */}
                 </div>
                 <div className="modal-body">
-                    <img src={props.data.recipe.image} alt="src\assets\placeholder.gif" />
-                    
+                    <img src={recipe.image} alt="src\assets\placeholder.gif" />
+
                     <div className="modal-section show-allergy">
                         <p className="modal-section-label">Dietary Info</p>
                         <ul>
@@ -39,6 +42,17 @@ const RecipeDetails = (props) => {
                                 <li key={label}>{label}</li>
                             ))}
                         </ul>
+                    </div>
+
+                    <div className="modal-section">
+                        <p className="modal-section-label">Ingredients</p>
+                        <div className="modal-list">
+                            {ingredientInfo.map(ingredients => (
+                                <ul key={uuid()}>
+                                    <li>{ingredients}</li>
+                                </ul>
+                            ))}
+                        </div>
                     </div>
 
                     <div className="modal-section">
@@ -56,22 +70,11 @@ const RecipeDetails = (props) => {
                             ))}
                         </div>
                     </div>
-
-                    <div className="modal-section">
-                        <p className="modal-section-label">Ingredients</p>
-                        <div className="modal-list">
-                            {props.data.recipe.ingredientLines.map(ingredients => (
-                                <ul key={uuid()}>
-                                    <li>{ingredients}</li>
-                                </ul>
-                            ))}
-                        </div>
-                    </div>
                 </div>
 
                 <div className="recipe-link">
                     {/* <span className="recipe-link-dot" /> */}
-                    <a href={props.data.recipe.url} target="_blank" rel="noopener noreferrer" className="modal-full-recipe">Full recipe ↗</a>
+                    <a href={recipe.url} target="_blank" rel="noopener noreferrer" className="modal-full-recipe">Full recipe ↗</a>
                     <button onClick={props.onClose} className="modal-full-recipe">Close</button>
                 </div>
 
